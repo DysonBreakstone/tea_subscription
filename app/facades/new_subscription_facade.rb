9971104720fa4066caeac51d285883d71c514d9a
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-08-02 15:10:55 -0600

    create facades

---
+++ app/facades/new_subscription_facade.rb
@@ -0,0 +1,17 @@
+class NewSubscriptionFacade
+  def self.create_subscriptions(id, sub)
+    subscription = Subscription.new(customer_id: id, title: sub[:title], status: true, frequency: sub[:frequency])
+    if subscription.save
+      sub[:teas].each do |tea, quant|
+        sub_tea = SubscriptionTea.new(subscription_id: subscription.id, tea_id: tea, quantity: quant)
+        if sub_tea.save
+          next
+        else
+          return false
+        end
+      end
+    else
+      return false
+    end
+  end
+end
