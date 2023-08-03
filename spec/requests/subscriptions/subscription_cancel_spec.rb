90911fc60f89068c7c8e78ab96e8a887ed7b4ac4
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-08-03 10:24:44 -0600

    cancel subscription functional

--- spec/requests/subscriptions/subscription_cancel_spec.rb
+++ spec/requests/subscriptions/subscription_cancel_spec.rb
@@ -8,20 +8,20 @@ RSpec.describe "cancel" do
 
     it "successful response" do
       put "/customers/#{@customer_10.id}/subscriptions/#{@subscription_19.id}", params: {status: false}
-      json = JSON.parse(response.body, symbolize_names: true)
+      # require 'pry'; binding.pry
+      # json = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to have_http_status(204)
-      expect(json[:data].first[:type]).to eq("subscriptions")
+      # expect(json[:data].first[:type]).to eq("subscriptions")
     end
 
     it "cancels subscription" do
-      expect(@customer_7.subscriptions.where({ status: false })).to eq(1)
+      expect(@customer_7.subscriptions.where({ status: false }).size).to eq(1)
       
       put "/customers/#{@customer_7.id}/subscriptions/#{@subscription_19.id}", params: {status: false}
 
       @customer_7.reload
-      expect(@customer_7.subscriptions.where({ status: false })).to eq(2)
-
+      expect(@customer_7.subscriptions.where({ status: false }).size).to eq(2)
     end
   end
 end
